4d6dd99e0340de3455b80876e2e598293919806c
Vahe Abrahamyan <vavabrahamyan@gmail.com>
2020-06-03 01:10:24 +0400

    4-cd

---
+++ AspNetCoreMVC4Me/DataLayer/EFDBContext.cs
@@ -0,0 +1,26 @@
+﻿using DataLayer.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataLayer
+{
+    public class EFDBContext:DbContext
+    {
+        public DbSet<Directory> Directory { get; set; }
+        public DbSet<Material> Materials { get; set; }
+        public EFDBContext(DbContextOptions<EFDBContext> options) : base(options) { }
+    }
+    public class EFDBContextFactory : IDesignTimeDbContextFactory<EFDBContext>
+    {
+        public EFDBContext CreateDbContext(string[] args)
+        {
+            var optionBuilder = new DbContextOptionsBuilder<EFDBContext>();
+            optionBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;DataBase=LoftSoftAspNetCoreMVC; Trusted_Connection=true; MultipleAvtiveResultSets=true", b => b.MigrationsAssembly("DataLayer"));
+
+            return new EFDBContext(optionBuilder.Options);
+        }
+    }
+}
---
+++ AspNetCoreMVC4Me/DataLayer/Entities/Material.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataLayer.Entities
+{
+    public class Material:Page
+    {
+        public int DIrectoryId { get; set; }
+        public Directory Directiry { get; set; }
+    }
+}
