3e8a3207c26241f56c5585f96875c010e2b778dc
Vahe Abrahamyan <vavabrahamyan@gmail.com>
2020-06-02 23:28:43 +0400

    1-cd

---
+++ AspNetCoreMVC4Me/LoftBlogLeranAspNetCoreMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace LoftBlogLeranAspNetCoreMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
